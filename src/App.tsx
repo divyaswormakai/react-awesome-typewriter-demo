0d76520baf8177eb7a087e68ef2761a82d9a86c9
Divyaswor Makai <makaidivya@gmail.com>
2023-10-11 13:24:40 +0545

    feat: minor updates for ui

--- src/App.tsx
+++ src/App.tsx
@@ -14,20 +14,54 @@ function App() {
       { text: "Big Text ", classNames: "text-big" },
       { text: "Small Text", classNames: "text-small" },
     ],
+    [
+      {
+        text: "Small Secondary Text ",
+        classNames: "text-small text-secondary",
+      },
+      { text: "Big Primary Text ", classNames: "text-big text-primary" },
+    ],
   ]);
 
   return (
     <>
+      <a
+        href="https://www.npmjs.com/package/react-awesome-typewriter"
+        target="_blank"
+      >
+        <img src="https://img.shields.io/badge/package-npm-red?style=flat-square&logo=npm" />
+      </a>
       <h1>
         <ReactAwesomeTypewriter
           options={[[{ text: "React Awesome Typewriter" }]]}
         />
       </h1>
-      <div style={{ height: "200px" }}>
+      <div style={{ height: "80px" }}>
         <p>
           <ReactAwesomeTypewriter options={options} cursorGap="5px" />
         </p>
       </div>
+
+      <p className="text-small">
+        <a
+          href="https://www.npmjs.com/package/react-awesome-typewriter"
+          target="_blank"
+        >
+          Visit package
+        </a>
+      </p>
+
+      <p style={{ display: "flex", justifyContent: "center" }}>
+        Built by:{" "}
+        <a
+          href="https://www.divyaswormakai.com.np"
+          target="_blank"
+          style={{ paddingRight: "5px", paddingLeft: "5px" }}
+        >
+          Divyaswor Makai Shrestha
+        </a>
+        with ❤️
+      </p>
     </>
   );
 }
